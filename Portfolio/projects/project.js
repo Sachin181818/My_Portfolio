7d8255c778c834decf6e7052bde8e770bb630249
Sachin181818 <sachinsmp02@gmail.com>
2022-05-31 19:41:25 +0530

    portfolio

---
+++ Portfolio/projects/project.js
@@ -0,0 +1,149 @@
+const Projects = [
+    {
+        name : "Clone of Travelocity.com",
+        about : " Travelocity is an online travel agency owned by Expedia Group. It has 12.4 million monthly unique visitors, making it the third most popular website owned by Expedia Group, after Expedia.com and Hotels.com. U.S",
+        img : "./images/Travel.png",
+        gitrepo : "https://github.com/Sachin181818/React_Travelocity_Clone",
+         
+        tech_stack : "HTML | CSS | JAVASCRIPT | React | Redux | Nodejs",
+        tech_stackicons : [
+            `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+            `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+            `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+            `<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+            `<img src="https://img.icons8.com/color/48/000000/redux.png"/>`,
+            `<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+         
+        ],
+        project_link : "https://celadon-dodol-b03f1e.netlify.app",
+        project_type : "Team Project"
+    },
+    {
+        name : "Clone of Udemy.com",
+        about : "Udemy is an online learning platform that helps students, companies, and governments gain the skills they need to reach their goals.",
+        img : "./images/Udemy.png",
+        gitrepo : "https://github.com/Sachin181818/Udemy-clone",
+     
+
+        tech_stack : "HTML | CSS | JAVASCRIPT | API | JSON-SERVER",
+        tech_stackicons : [
+        `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+        `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+        `<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+        `<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`
+        ],
+        project_link : "https://verdant-kitten-680f9d.netlify.app",
+        project_type : "Team Project"
+    },
+    {
+        name : "Clone of PharmEasy.in",
+        about : "PharmEasy is a health tech startup offering services such as teleconsultation, medicine deliveries, and diagnostic test sample collections.",
+        img : "./images/Pharm.png",
+        gitrepo : "https://github.com/Sachin181818/Pharmeasy",
+     
+
+        tech_stack : "HTML | CSS | JAVASCRIPT ",
+        tech_stackicons : [
+        `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+        `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+        ],
+        project_link : "https://friendly-wozniak-6486dd.netlify.app/",
+        project_type : "Team Project"
+    }
+    // {
+    //     name : "Clone of shop4reebok.com",
+    //     about : "An E-commerce website that sells fashionable, branded clothing & footwear and accessories for women, men & kids online at shop4reebok.",
+    //     img : "https://content.shop4reebok.com/static/hero/Sneeker_Fest_1920x800_091221_2.jpg",
+    //     gitrepo : "https://github.com/db648/reebok",
+    //     blog : "https://medium.com/@suraj.devere/shop4reebok-clone-eddfa7c32fe7",
+    //     tech_stack : "HTML | CSS | JAVASCRIPT",
+    //     tech_stackicons : [
+    //         `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+    //         `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+    //         `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+            
+    //     ],
+    //     project_link : "https://rebookwebsite-clone1.vercel.app/",
+    //     project_type : "Team Project"
+
+    // },
+    // {
+    //     name : "Clone of MyShopping.com",
+    //     about : "An E-commerce website that sells fashionable, branded clothing for women, men & kids online at MyShopping.com.",
+    //     img : "./homepage-shopping.png",
+    //     gitrepo : "https://github.com/db648/ecommerce-react-project",
+    //     blog : "",
+    //     tech_stack : "HTML | CSS | JAVASCRIPT | React | Redux | MongoDB | Express | Nodejs | Bootstrap",
+    //     tech_stackicons : [
+    //         `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+    //         `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+    //         `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+    //         `<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+    //         `<img src="https://img.icons8.com/color/48/000000/redux.png"/>`,
+    //         `<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+    //         `<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
+    //         `<img src="https://img.icons8.com/color/48/000000/express.png"/>`,
+    //         `<img src="https://img.icons8.com/color/48/000000/bootstrap.png"/>`
+            
+    //     ],
+    //     project_link : "https://ecommerce-react-project.vercel.app/",
+    //     project_type : "Individual Project"
+
+    // },
+    // {
+    //     name : "Weather-Map Web Application",
+    //     about : "A Weather search web application, where user will get location based weather report. Here user will get all info related to a particular city with daily report as well as weekly forecast.",
+    //     img : "./weather.png",
+    //     gitrepo : "https://github.com/db648/weatherapp",
+    //     blog : "",
+    //     tech_stack : "HTML | CSS | JAVASCRIPT | API's",
+    //     tech_stackicons : [
+    //         `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+    //         `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+    //         `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+    //         `<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`
+            
+    //     ],
+    //     project_link : "https://weatherapp-ten-kappa.vercel.app/",
+    //     project_type : "Individual Project"
+
+    // }
+]
+
+
+let projects__container = document.getElementById("projects--container")
+
+Projects.forEach(pro => {
+    let main = document.createElement("div")
+    main.setAttribute("class", "portfolio-item padd-15")
+    main.innerHTML = `
+    <div class="portfolio-item-inner shadow-dark">
+    <div class="portfolio-img">
+      <img src=${pro.img} alt="">
+      <h3 class="project-name"><b>${pro.name}</b></h3> <br/>
+      <h4 class="project-name project__about">${pro.about}</h4>
+      <a href=${pro.project_link} target="_blank" class="see-project">Live💻</a>
+      <a href=${pro.gitrepo} target="_blank" class="github-repo">GitHub</a>
+      </div>
+      <br/>
+      <h3 class="project-name techcen"><b>Tech Stack</b></h3>
+      <span>${pro.tech_stackicons[0] ? pro.tech_stackicons[0] : '' }
+      ${pro.tech_stackicons[1] ? pro.tech_stackicons[1] : ''}
+      ${pro.tech_stackicons[2] ? pro.tech_stackicons[2] : ''}
+      ${pro.tech_stackicons[3] ? pro.tech_stackicons[3] : ''}
+      ${pro.tech_stackicons[4] ? pro.tech_stackicons[4] : ''}
+      ${pro.tech_stackicons[5] ? pro.tech_stackicons[5] : ''}
+      ${pro.tech_stackicons[6] ? pro.tech_stackicons[6] : ''}
+      ${pro.tech_stackicons[7] ? pro.tech_stackicons[7] : ''}
+      ${pro.tech_stackicons[8] ? pro.tech_stackicons[8] : ''}
+      ${pro.tech_stackicons[9] ? pro.tech_stackicons[9] : ''}
+      </span>
+      
+    </div>
+  </div>
+    `
+    projects__container.append(main)
+
+})
